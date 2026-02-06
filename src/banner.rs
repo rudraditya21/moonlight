c0a6e7e4b4bd0b11bda55ca5f9562367d701f80d
Rudraditya Thakur <rudraditya.thakur21@gmail.com>
2026-02-06 14:40:11 +0530

    updated: banner

--- src/banner.rs
+++ src/banner.rs
@@ -1,5 +1,21 @@
 pub fn print() {
-    println!("============================================================");
-    println!("  Moonlight - Metasploit-style framework in Rust (alpha)");
-    println!("============================================================");
+    let lines = [
+        r"   _____                       .__  .__       .__     __   ",
+        r"  /     \   ____   ____   ____ |  | |__| ____ |  |___/  |_ ",
+        r" /  \ /  \ /  _ \ /  _ \ /    \|  | |  |/ ___\|  |  \   __\",
+        r"/    Y    (  <_> |  <_> )   |  \  |_|  / /_/  >   Y  \  |  ",
+        r"\____|__  /\____/ \____/|___|  /____/__\___  /|___|  /__|  ",
+        r"        \/                   \/       /_____/      \/      ",
+    ];
+    if std::env::var("NO_COLOR").is_ok() {
+        for line in lines {
+            println!("{line}");
+        }
+        return;
+    }
+    let blue = "\x1b[34m";
+    let reset = "\x1b[0m";
+    for line in lines {
+        println!("{blue}{line}{reset}");
+    }
 }
